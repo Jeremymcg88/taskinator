51a8ad6ace508e3e4eb6bfafa6d110846de3d2b3
Jeremymcg88 <jeremy.mcguirk@gmail.com>
2021-07-20 03:55:11 -0400

    added js. now about to add input value to text box and display it.

--- project/assets/js/script.js
+++ project/assets/js/script.js
@@ -1,11 +1,31 @@
-var buttonEl = document.querySelector('#save-task');
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector ('#task-to-do');
 
-var createTaskHandler = function () {
-    var listItemEl = document.createElement('li');
-listItemEl.className = 'task-item';
-listItemEl.textContent = 'This is a new task';
-tasksToDoEl.appendChild(listItemEl)
-}
+var createTaskHandler = function (event) {
 
-buttonEl.addEventListener('click', createTaskHandler);
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+    // create list item
+    var listItemEl = document.createElement('li');
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+ // add HTML content to div
+ taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  
+ listItemEl.appendChild(taskInfoEl);
+ 
+ // add entire list item to list
+ tasksToDoEl.appendChild(listItemEl);
+    
+ var listItemEl = document.createElement('li');
+listItemEl.className = 'task-item';
+listItemEl.textContent = taskNameInput;
+};
+
+formEl.addEventListener('submit', createTaskHandler);
